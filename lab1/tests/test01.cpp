c95ab8ec8cd496b30410dc09eaab537311330feb
root <root@Kamila>
2024-09-21 13:14:06 +0300

    make available format of tests

--- lab1/tests/test01.cpp
+++ lab1/tests/test01.cpp
@@ -3,22 +3,30 @@
 
 TEST(test_01, basic_test_set)
 {
-    ASSERT_TRUE(findnum("123dddf2fff56")=="123 2 56");
+    std::string str = "123dddf2fff56";
+    std::string result = findnum(str);
+    ASSERT_TRUE(result == "123 2 56");
 }
 
 TEST(test_02, basic_test_set)
 {
-    ASSERT_TRUE(findnum("ffhhhf")=="");
+    std::string str = "ffhhhf";
+    std::string result = findnum(str);
+    ASSERT_TRUE(result =="");
 }
 
 TEST(test_03, basic_test_set)
 {
-    ASSERT_TRUE(findnum("d3fr22ff")=="3 22 ");
+    std::string str = "d3fr22ff";
+    std::string result = findnum(str);
+    ASSERT_TRUE(result =="3 22 ");
 }
 
 TEST(test_04, basic_test_set)
 {
-    ASSERT_TRUE(findnum("1456")=="1456");
+    std::string str = "1456";
+    std::string result = findnum(str);
+    ASSERT_TRUE(result =="1456");
 }
 
 
